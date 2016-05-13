e7e2c2870377f45d140614e03b78c0c8241b5706
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2016-05-13 16:05:40 +0200

    fixed loading container

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 CONTAINER=$1
-docker run --privileged -p 8080:8080 -p 8443:8443 -d ${CONTAINER}
+docker run --privileged -p 8080:8080 -p 8443:8443 -d -v /Users/i319397/gitHub/docker-chef_server/bookshelf:/bookshelf ${CONTAINER}
 #-v bookshelf:/var/opt/chef-server/bookshelf/data/bookshelf
